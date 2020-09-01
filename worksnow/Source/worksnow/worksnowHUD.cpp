eae2ce28d03447eae505853eafa00a5c487c7e45
Munghard <47939872+Munghard@users.noreply.github.com>
2020-09-02 01:02:00 +0300

    redingdong
    attempt to fix

--- worksnow/Source/worksnow/worksnowHUD.cpp
+++ worksnow/Source/worksnow/worksnowHUD.cpp
@@ -10,7 +10,7 @@
 AworksnowHUD::AworksnowHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT(""));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
