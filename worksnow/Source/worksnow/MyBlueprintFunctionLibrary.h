bc3cdfb24895b83ab54e455ad895bf3924adbcdc
Munghard <47939872+Munghard@users.noreply.github.com>
2020-11-05 00:52:07 +0200

    penisassman

---
+++ worksnow/Source/worksnow/MyBlueprintFunctionLibrary.h
@@ -0,0 +1,5 @@
+/** returns a list of all save games in /Saved/SaveGames folder, without the .sav extension (filename only) */
+/*
+UFUNCTION(BlueprintPure, Category = Game)
+static TArray<FString> GetAllSaveGameSlotNames();
+/*
---
+++ worksnow/Source/worksnow/worksnow.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "worksnow.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, worksnow, "worksnow" );
